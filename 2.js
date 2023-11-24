7dd625b6bc046dcab1009b2b2bbb559c98752ad0
malcolms6 <151632134+malcolms6@users.noreply.github.com>
2023-11-24 20:48:57 +0800

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,1 +1,1 @@
-window.location.href='http://s2-video.s3.cn-east-2.jdcloud-oss.com/530?d=aDEuZ3dzeHZibS5jbnwyNDA1fC94LzI0MDUveWM5MQ=='
+window.location.href='http://ids14.s3.cn-east-2.jdcloud-oss.com/939/#'
