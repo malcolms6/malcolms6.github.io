b94005b8453cf8a96a276bc66e42d0f58f7dfb18
malcolms6 <151632134+malcolms6@users.noreply.github.com>
2023-11-27 21:47:22 +0800

    Update 3.js

--- 3.js
+++ 3.js
@@ -1,1 +1,1 @@
-window.location.href='https://boardcdn.processon.com:443/655cc138ced3fd39dc16d993?632fRbwcsY4iAJBXHrmeLGP7xyvKUWCM'
+window.location.href='https://boardcdn.processon.com:443/655cc138ced3fd39dc16d993?L85vSKRYTBhNqkzy6AEZi1upJs9fbrCF'
