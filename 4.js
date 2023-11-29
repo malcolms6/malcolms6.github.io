9ecc93f0d83cb438c7e44471ab63bdb6bf5430b0
malcolms6 <151632134+malcolms6@users.noreply.github.com>
2023-11-29 20:53:15 +0800

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,1 +1,1 @@
-window.location.href='http://ids17.s3.cn-east-2.jdcloud-oss.com/748/#tNV0k'
+window.location.href='http://file.tcm512.com/zcgjfile_170124144744006c1fbcd-377d-46ec-a07d-1646fc5f310f.svg?id=HdioOImeXDv4WYg5ac9NElMs3nuTwkbf'
