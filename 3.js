71f12c995b029cf67ed22c8d87498ca182ed1b6e
malcolms6 <151632134+malcolms6@users.noreply.github.com>
2023-12-02 21:38:33 +0800

    Update 3.js

--- 3.js
+++ 3.js
@@ -1,1 +1,1 @@
-window.location.href='http://file.tcm512.com/zcgjfile_1701241452330fe8f3877-15a4-492a-88e9-737602c06b15.svg?id=jF0Y1vpyhLZs2B76MINSCiPXfxET3VJk'
+window.location.href='https://maobb.su.bcebos.com/Tesla?id=753qWOdSnMraLicXH4f6xeG2R0y8mQbT'
