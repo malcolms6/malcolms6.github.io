d0d0c30643fcc448aa46be05cbc596f2387b12ef
malcolms6 <151632134+malcolms6@users.noreply.github.com>
2023-12-20 15:24:50 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,1 +1,1 @@
-window.location.href='https://maobb.cdn.bcebos.com/Tesla?id=NeU27dD3pTrlCsikBYP4vOuaXE9AmJSH'
+window.location.href='http://xianzhe8.tos-cn-guangzhou.volces.com/t-y?code=MN8iDf8angY&815'
